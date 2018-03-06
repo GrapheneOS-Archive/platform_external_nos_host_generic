08c561d8f052e51e8e8f0d7cb91c6aae5057d216
Bill Richardson <wfrichar@google.com>
2018-03-06 13:13:16 -0800

    Add LOG_TAG for libnos_transport functions
    So they're easier to spot in the logcat output
    
    Change-Id: Ia7928ba7de9b92a164af343825a2a659db51b32e
    Signed-off-by: Bill Richardson <wfrichar@google.com>

--- libnos_transport/transport.c
+++ libnos_transport/transport.c
@@ -32,12 +32,13 @@
 
 #ifdef ANDROID
 /* Logging for Android */
+#define LOG_TAG "libnos_transport"
 #include <sys/types.h>
-#include <log/log.h>
+#include <cutils/log.h>
 
 #define NLOGE(...) ALOGE(__VA_ARGS__)
-#define NLOGV(...) do { if (VERBOSE_LOG) { ALOGV(__VA_ARGS__); } } while (0)
-#define NLOGD(...) do { if (DEBUG_LOG) { ALOGD(__VA_ARGS__); } } while (0)
+#define NLOGV(...) ALOGV(__VA_ARGS__)
+#define NLOGD(...) ALOGD(__VA_ARGS__)
 
 extern int usleep (uint32_t usec);
 
@@ -83,7 +84,6 @@ static int clear_status(const struct nos_device *dev, uint8_t app_id)
   return 0;
 }
 
-
 uint32_t nos_call_application(const struct nos_device *dev,
                               uint8_t app_id, uint16_t params,
                               const uint8_t *args, uint32_t arg_len,
@@ -124,8 +124,7 @@ uint32_t nos_call_application(const struct nos_device *dev,
     if (get_status(dev, app_id, &status, &ulen) != 0) {
       return APP_ERROR_IO;
     }
-    NLOGV("%d: query status 0x%08x  ulen 0x%04x\n",__LINE__,
-          status, ulen);
+    NLOGV("%d: query status 0x%08x  ulen 0x%04x",__LINE__, status, ulen);
 
     /* It's ignoring us and is still not ready, so it's broken */
     if (status != APP_STATUS_IDLE) {
@@ -150,7 +149,7 @@ uint32_t nos_call_application(const struct nos_device *dev,
     if (args && ulen)
       memcpy(buf, args, ulen);
 
-    NLOGV("Write command 0x%08x, bytes 0x%x\n", command, ulen);
+    NLOGV("Write command 0x%08x, bytes 0x%x", command, ulen);
 
     if (0 != dev->ops.write(dev->ctx, command, buf, ulen)) {
       NLOGE("Failed to send datagram to device");
@@ -170,14 +169,14 @@ uint32_t nos_call_application(const struct nos_device *dev,
   if (get_status(dev, app_id, &status, &ulen) != 0) {
     return APP_ERROR_IO;
   }
-  NLOGV("%d: query status 0x%08x  ulen 0x%04x\n", __LINE__, status, ulen);
+  NLOGV("%d: query status 0x%08x  ulen 0x%04x", __LINE__, status, ulen);
   if (status & APP_STATUS_DONE)
     /* Yep, problems. It should still be idle. */
     goto reply;
 
   /* Now tell the app to do whatever */
   command = CMD_ID(app_id) | CMD_PARAM(params);
-  NLOGV("Write command 0x%08x\n", command);
+  NLOGV("Write command 0x%08x", command);
   if (0 != dev->ops.write(dev->ctx, command, 0, 0)) {
       NLOGE("Failed to send command datagram to device");
       return APP_ERROR_IO;
@@ -188,11 +187,10 @@ uint32_t nos_call_application(const struct nos_device *dev,
     if (get_status(dev, app_id, &status, &ulen) != 0) {
       return APP_ERROR_IO;
     }
-    NLOGD("%d:  poll status 0x%08x  ulen 0x%04x\n", __LINE__,
-          status, ulen);
+    NLOGD("%d:  poll status 0x%08x  ulen 0x%04x", __LINE__, status, ulen);
     poll_count++;
   } while (!(status & APP_STATUS_DONE));
-  NLOGV("polled %d times, status 0x%08x  ulen 0x%04x\n", poll_count,
+  NLOGV("polled %d times, status 0x%08x  ulen 0x%04x", poll_count,
         status, ulen);
 
 reply:
@@ -213,8 +211,7 @@ reply:
        * a constant. For other buses, it may not be.
        */
       gimme = MIN(left, MAX_DEVICE_TRANSFER);
-      NLOGV("Read command 0x%08x, bytes 0x%x\n",
-            command, gimme);
+      NLOGV("Read command 0x%08x, bytes 0x%x", command, gimme);
       if (0 != dev->ops.read(dev->ctx, command, buf, gimme)) {
         NLOGE("Failed to receive datagram from device");
         return APP_ERROR_IO;
