6f09c47236d03ef300d20521d360e89fa39f8e7d
Allen Webb <allenwebb@google.com>
2018-03-30 14:55:29 -0700

    Follow up to format string fixes.
    Change-Id: Ie032704e9c76e9b0efefeeaf2594423bbb1b3dc0

--- citadel/updater/updater.cpp
+++ citadel/updater/updater.cpp
@@ -387,15 +387,16 @@ uint32_t do_stats(AppClient &app)
 
     memcpy(&stats, buffer.data(), sizeof(stats));
 
-    printf("hard_reset_count         "PRIu64"\n", stats.hard_reset_count);
-    printf("time_since_hard_reset    "PRIu64"\n", stats.time_since_hard_reset);
-    printf("wake_count               "PRIu64"\n", stats.wake_count);
-    printf("time_at_last_wake        "PRIu64"\n", stats.time_at_last_wake);
-    printf("time_spent_awake         "PRIu64"\n", stats.time_spent_awake);
-    printf("deep_sleep_count         "PRIu64"\n", stats.deep_sleep_count);
-    printf("time_at_last_deep_sleep  "PRIu64"\n",
+    printf("hard_reset_count         %" PRIu64 "\n", stats.hard_reset_count);
+    printf("time_since_hard_reset    %" PRIu64 "\n",
+           stats.time_since_hard_reset);
+    printf("wake_count               %" PRIu64 "\n", stats.wake_count);
+    printf("time_at_last_wake        %" PRIu64 "\n", stats.time_at_last_wake);
+    printf("time_spent_awake         %" PRIu64 "\n", stats.time_spent_awake);
+    printf("deep_sleep_count         %" PRIu64 "\n", stats.deep_sleep_count);
+    printf("time_at_last_deep_sleep  %" PRIu64 "\n",
            stats.time_at_last_deep_sleep);
-    printf("time_spent_in_deep_sleep "PRIu64"\n",
+    printf("time_spent_in_deep_sleep %" PRIu64 "\n",
            stats.time_spent_in_deep_sleep);
   }
 
